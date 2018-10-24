5bdc6f64993bac5f597bfa83a88d41c1f677a4af
Henrique Silva <jhenrique09.mcz@hotmail.com>
2018-10-24 11:55:31 -0300

    whyred: Update MiuiCamera repo path

--- aosp_whyred.mk
+++ aosp_whyred.mk
@@ -23,7 +23,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_n_mr1.mk
 $(call inherit-product, device/xiaomi/whyred/device.mk)
 
 # Inherit from custom vendor
-$(call inherit-product, vendor/xiaomi/MiuiCamera/config.mk)
+$(call inherit-product, vendor/MiuiCamera/config.mk)
 
 # Inherit some common PixelExperience stuff.
 TARGET_BOOT_ANIMATION_RES := 1080
